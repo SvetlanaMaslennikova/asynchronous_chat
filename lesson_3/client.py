0ade853e7ded48e552a5e789704cd120923ab909
Svetlana Maslennikova <svetlana-pavlova1005@mail.ru>
2023-04-25 13:23:42 +0300

    Lesson_3

---
+++ lesson_3/client.py
@@ -0,0 +1,63 @@
+from socket import *
+import time
+import json
+import sys
+
+
+def socket_init():
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    return client_socket
+
+
+def socket_connect(client_socket, address, port):
+    client_socket.connect((address, port))
+
+    presence = make_json_byte_presence()
+    client_socket.send(presence)
+
+    response = client_socket.recv(1024)
+    client_socket.close()
+    return response.decode('unicode_escape')
+
+
+def make_json_byte_presence():
+    presence = {
+        "action": "presence",
+        "time": time.time(),
+        "type": "status",
+        "user": {
+            "account_name": "C0deMaver1ck",
+            "status": "Yep, I am here"
+        }
+    }
+    return json.dumps(presence).encode('utf-8')
+
+
+def get_args(args):
+    address = 'localhost'
+    try:
+        address = str(args[1])
+    except Exception:
+        print('No address value. Address set to "localhost"')
+
+    port = 7777
+    try:
+        port = int(args[2])
+    except Exception:
+        print('No port value. Port set to "7777"')
+
+    return address, port
+
+
+def main():
+    args = sys.argv
+    address, port = get_args(args)
+    print(f'{address=} {port=}')
+
+    client_socket = socket_init()
+    server_response = socket_connect(client_socket, address, port)
+    print(f'{server_response=}')
+
+
+if __name__ == '__main__':
+   main()
